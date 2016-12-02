86221435ee54db49d93356a0b26c43a9c1bfe693
igormulyar <igormulyar@ukr.net>
2016-12-02 16:35:22 +0200

    Initial commit

---
+++ src/main/java/HelloWorldServlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by TRUST on 02.12.2016.
+ */
+
+@WebServlet(name="helloServlet", urlPatterns={"/hello"})
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter pw = resp.getWriter();
+        pw.println("<H1>Ээээ.. Мой первый сервлет, ёпта!..</H1>");
+
+    }
+}
